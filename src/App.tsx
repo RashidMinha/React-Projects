8ceeec4f0cc63d0f54866e417f22cd317433771a
RashidMinhas37 <www.rashidminhaswebuser37@gmail.com>
2022-10-25 13:28:18 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from './components/h/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+     <Dashboard Name="RashidMinhas" age={23} occupation="Font-End Developer" color="red" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/h/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+export default function Dashboard({Name , age , occupation,color}:any) {
+  return (
+    <div>
+       <h2>{Name}</h2>
+       <h2>{age}</h2>
+       <p style={color={color}}>{occupation}</p>
+    </div>
+  )
+}
